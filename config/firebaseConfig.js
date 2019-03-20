6343b4e4b2431b2d3824ca791c0dacefb2afae75
FilipeVeber <filipeveber73@gmail.com>
2019-03-19 23:27:28 -0300

    First commit

---
+++ config/firebaseConfig.js
@@ -0,0 +1,40 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const config = {
+    apiKey: "AIzaSyD3cah5h1Tc_mwmifwlvEzmz7Un-aUUbJo",
+    authDomain: "savvy-project-c8463.firebaseapp.com",
+    databaseURL: "https://savvy-project-c8463.firebaseio.com",
+    projectId: "savvy-project-c8463",
+    storageBucket: "savvy-project-c8463.appspot.com",
+    messagingSenderId: "815465239689"
+};
+
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+const currentUser = auth.currentUser
+
+// date issue fix according to firebase
+const settings = {
+    timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// firebase collections
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+export {
+    db,
+    auth,
+    currentUser,
+    usersCollection,
+    postsCollection,
+    commentsCollection,
+    likesCollection
+}
