bec18ebabc9198df9fd222fd4ce3aeb93d852616
FilipeVeber <filipeveber73@gmail.com>
2019-03-25 23:11:18 -0300

    Firebase app import call issue fixed. Form to reset password

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="login">
+    <transition name="fade">
+      <div v-if="performingRequest" class="loading">
+        <p>Loading...</p>
+      </div>
+    </transition>
     <section>
       <div class="col1">
         <h1>Vuegram</h1>
@@ -9,7 +14,7 @@
           Build this project by checking out The Definitive Guide to Getting Started with Vue.js
         </p>
       </div>
-      <div class="col2" :class="{ 'signup-form': !showLoginForm }">
+      <div class="col2" :class="{ 'signup-form': !showLoginForm && !showForgotPassword }">
         <form v-if="showLoginForm" @submit.prevent>
           <h1>Welcome Back</h1>
 
@@ -27,12 +32,12 @@
           <button @click="login" class="button">Log In</button>
 
           <div class="extras">
-            <a>Forgot Password</a>
+            <a @click="togglePasswordReset">Forgot Password</a>
             <a @click="toggleForm">Create an Account</a>
           </div>
         </form>
 
-        <form v-else @submit.prevent>
+        <form v-if="!showLoginForm && !showForgotPassword" @submit.prevent>
           <h1>Get Started</h1>
 
           <label for="name">Name</label>
@@ -63,6 +68,38 @@
             <a @click="toggleForm">Back to Log In</a>
           </div>
         </form>
+
+        <form v-if="showForgotPassword" @submit.prevent class="password-reset">
+          <div v-if="!passwordResetSuccess">
+            <h1>Reset Password</h1>
+            <p>We will send you an email to reset your password</p>
+
+            <label for="email3">Email</label>
+            <input
+              v-model.trim="passwordForm.email"
+              type="text"
+              placeholder="your@email.com"
+              id="email3"
+            >
+
+            <button @click="resetPassword" class="button">Submit</button>
+
+            <div class="extras">
+              <a @click="togglePasswordReset">Back to login</a>
+            </div>
+          </div>
+          <div v-else>
+            <h1>Email Sent</h1>
+            <p>Check your email for a link to reset your password</p>
+            <button @click="togglePasswordReset" class="button">Back to login</button>
+          </div>
+        </form>
+
+        <transition name="fade">
+          <div v-if="errorMsg !== ''" class="error-msg">
+            <p>{{ errorMsg }}</p>
+          </div>
+        </transition>
       </div>
     </section>
   </div>
@@ -84,14 +121,19 @@ export default {
         email: "",
         password: ""
       },
-      showLoginForm: true
+      showLoginForm: true,
+      performingRequest: false,
+      errorMsg: ""
     };
   },
   methods: {
     toggleForm() {
+      this.errorMsg = "";
       this.showLoginForm = !this.showLoginForm;
     },
     login() {
+      this.performingRequest = true;
+
       fb.auth
         .signInWithEmailAndPassword(
           this.loginForm.email,
@@ -100,20 +142,25 @@ export default {
         .then(user => {
           this.$store.commit("setCurrentUser", user);
           this.$store.dispatch("fetchUserProfile");
+          this.performingRequest = false;
           this.$router.push("/dashboard");
         })
         .catch(error => {
+          this.errorMsg = error;
           console.log(error);
+          this.performingRequest = false;
         });
     },
     signup() {
+      this.performingRequest = true;
+
       fb.auth
         .createUserWithEmailAndPassword(
           this.signupForm.email,
-          signupForm.password
+          this.signupForm.password
         )
         .then(user => {
-          this.$store.commit("setCurrentUser", user);
+          this.$store.commit("setCurrentUser", user.user);
 
           // create user object
           fb.usersCollection
@@ -124,9 +171,11 @@ export default {
             })
             .then(() => {
               this.$store.dispatch("fetchUserProfile");
-              this.$strore.push("/dashboard");
+              this.performingRequest = true;
+              this.$$router.push("/dashboard");
             })
             .catch(err => {
+              this.performingRequest = false;
               console.log(err);
             });
         })
